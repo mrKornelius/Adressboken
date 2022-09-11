8f0aeb366c72b35111a5313ef21ef16086d7b6d3
Gusbod <skrapiskrap@gmail.com>
2022-09-11 21:59:16 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿while (true)
+{
+    Console.WriteLine("ADRESSBOKEN");
+    Console.WriteLine("1. Visa kontakter");
+    Console.WriteLine("2. Lägg till kontakt");
+    Console.WriteLine("3. Avsluta");
+}
