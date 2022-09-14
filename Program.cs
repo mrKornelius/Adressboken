90f62f668db675e38962352976a9f34b2904bbc7
Gusbod <skrapiskrap@gmail.com>
2022-09-14 10:17:26 +0200

    Added names and numbers

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿string contacts = "Jonas Jonasson\nSven Svensson";
+string[] names = new string[0];
+string[] numbers = new string[0];
 
 while (true)
 {
@@ -12,14 +14,21 @@ while (true)
     if (choice == '1')
     {
         Console.WriteLine("KONTAKTER");
-        Console.WriteLine(contacts);
+        for (int i = 0; i < names.Length; i++)
+        {
+            Console.WriteLine(names[i]);
+        }
         Console.ReadLine();
     }
     else if (choice == '2')
     {
         Console.WriteLine("LÄGG TILL KONTAKT");
         Console.Write("Namn: ");
-        contacts += Environment.NewLine + Console.ReadLine();
+        // contacts += Environment.NewLine + Console.ReadLine();
+        string name = Console.ReadLine();
+        Console.WriteLine("Nummer: ");
+        string number = Console.ReadLine();
+        AddContact(name, number);
     }
     else if (choice == '3')
     {
@@ -31,6 +40,17 @@ while (true)
         Console.WriteLine("Välj ett alternativ i menyn och tryck enter!");
         Console.ReadLine();
     }
+}
+
+void AddContact(string name, string number)
+{
+    string[] newNamesArray = new string[names.Length + 1];
+    for (int i = 0; i < names.Length; i++)
+    {
+        newNamesArray[i] = names[i];
+    }
+    names = newNamesArray;
+    names[^1] = name;
 }
 
 static char GetChar(string prompt)
